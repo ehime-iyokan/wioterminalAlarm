606b34140c958aa005d3f457bc4bf9a5331db7b6
ehime-iyokan <znkknz4@gmail.com>
2023-05-30 00:44:31 +0900

    時間を表示する処理を追加

--- go.mod
+++ go.mod
@@ -11,4 +11,5 @@ require (
 	fyne.io/fyne/v2 v2.2.3 // indirect
 	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/text v0.3.7 // indirect
+	tinygo.org/x/tinyfont v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/sago35/tinydisplay/examples/initdisplay"
+	"tinygo.org/x/tinyfont"
+	"tinygo.org/x/tinyfont/freemono"
 )
 
 var (
@@ -25,9 +28,27 @@ func main() {
 		log.Fatal(err)
 	}
 
+	glay := color.RGBA{R: 0x88, G: 0x88, B: 0x88, A: 0xFF}
+	black := color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xFF}
+	display.FillScreen(black)
+	YMDlabel := NewLabel(24, 320)
+	HMSlabel := NewLabel(24, 320)
+	YMDlabel.FillScreen(glay)
+	HMSlabel.FillScreen(glay)
+
 	for {
 		time.Sleep(1 * time.Second)
 		timeNow := fetchStringNowJst()
 		fmt.Println(timeNow)
+		array := strings.Split(timeNow, " ")
+		ymd := array[0]
+		hms := array[1]
+
+		YMDlabel.FillScreen(glay)
+		tinyfont.WriteLine(YMDlabel, &freemono.Regular12pt7b, 0, 18, ymd, white)
+		display.DrawRGBBitmap(0, 0, YMDlabel.Buf, YMDlabel.W, YMDlabel.H)
+		HMSlabel.FillScreen(glay)
+		tinyfont.WriteLine(HMSlabel, &freemono.Regular12pt7b, 0, 18, hms, white)
+		display.DrawRGBBitmap(0, 24, HMSlabel.Buf, HMSlabel.W, HMSlabel.H)
 	}
 }
