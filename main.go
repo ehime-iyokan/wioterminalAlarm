3a9822d709e34aeedef17bb74b7af2b94f72b6bd
ehime-iyokan <znkknz4@gmail.com>
2023-06-05 00:25:17 +0900

    関数名変更、コメント追加等

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ type CrossKey struct {
 
 func main() {
 	white := color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xFF}
+	glay := color.RGBA{R: 0x88, G: 0x88, B: 0x88, A: 0xFF}
+	black := color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xFF}
 
 	display := initdisplay.InitDisplay()
 	display.FillScreen(white)
@@ -36,8 +38,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	glay := color.RGBA{R: 0x88, G: 0x88, B: 0x88, A: 0xFF}
-	black := color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xFF}
 	display.FillScreen(black)
 	labelTimeNow := NewLabel(72, 320)
 	SettingAlermlabel := NewLabel(48, 320)
@@ -55,11 +55,11 @@ func main() {
 	channelA, _ := pwm.Channel(machine.BUZZER_CTR)
 	pwm.SetPeriod(uint64(1e9) / 440)
 
-	AlermON := 0
+	alermRinging := 0
 	button_2 := machine.BUTTON_2
 	button_2.Configure(machine.PinConfig{Mode: machine.PinInput})
 	button_2.SetInterrupt(machine.PinFalling, func(machine.Pin) {
-		AlermON = 0
+		alermRinging = 0
 		pwm.Set(channelA, 0)
 	})
 
@@ -70,14 +70,14 @@ func main() {
 		right: machine.SWITCH_Z,
 		left:  machine.SWITCH_Y,
 	}
-	// flgAlermSetting = 0:秒調整, 1:時間調整
-	flgAlermSetting := 0
+	// selectorAlermTime = 0:秒調整, 1:時間調整
+	selectorAlermTime := 0
 	alermMinute := 0
 	alermHour := 0
 
 	crosskey.up.Configure(machine.PinConfig{Mode: machine.PinInput})
 	crosskey.up.SetInterrupt(machine.PinFalling, func(machine.Pin) {
-		if flgAlermSetting == 0 {
+		if selectorAlermTime == 0 {
 			if 0 <= alermMinute && alermMinute < 59 {
 				alermMinute++
 			} else {
@@ -93,7 +93,7 @@ func main() {
 	})
 	crosskey.down.Configure(machine.PinConfig{Mode: machine.PinInput})
 	crosskey.down.SetInterrupt(machine.PinFalling, func(machine.Pin) {
-		if flgAlermSetting == 0 {
+		if selectorAlermTime == 0 {
 			if 0 < alermMinute && alermMinute <= 59 {
 				alermMinute--
 			} else {
@@ -110,14 +110,14 @@ func main() {
 	})
 	crosskey.left.Configure(machine.PinConfig{Mode: machine.PinInput})
 	crosskey.left.SetInterrupt(machine.PinFalling, func(machine.Pin) {
-		flgAlermSetting ^= 1
+		selectorAlermTime ^= 1
 	})
 	crosskey.right.Configure(machine.PinConfig{Mode: machine.PinInput})
 	crosskey.right.SetInterrupt(machine.PinFalling, func(machine.Pin) {
-		flgAlermSetting ^= 1
+		selectorAlermTime ^= 1
 	})
 
-	timeNow := fetchStringNowJst()
+	timeNow := time.Time{}
 	timeNowBefore := timeNow
 	timeAlermStringBefore := ""
 	modeBefore := 0
@@ -126,35 +126,39 @@ func main() {
 		timeNow = fetchStringNowJst()
 
 		if mode == 1 {
+			// 時間表示モード
 			if modeBefore == 0 {
+				// 画面遷移直後の処理
 				labelTimeNow.FillScreen(glay)
 			}
 
-			timeNowString := fmt.Sprintf("%04d/%02d/%02d\n%02d:%02d:%02d",
-				timeNow.Year(), timeNow.Month(), timeNow.Day(), timeNow.Hour(), timeNow.Minute(), timeNow.Second())
-
 			if timeNow.Hour() == alermHour && timeNow.Minute() == alermMinute {
-				AlermON = 1
+				alermRinging = 1
 				alermHour = 88
 				alermMinute = 88
 				pwm.Set(channelA, pwm.Top()/4)
 			}
 
-			if AlermON == 1 {
+			timeNowString := fmt.Sprintf("%04d/%02d/%02d\n%02d:%02d:%02d",
+				timeNow.Year(), timeNow.Month(), timeNow.Day(), timeNow.Hour(), timeNow.Minute(), timeNow.Second())
+			if alermRinging == 1 {
 				timeNowString = timeNowString + "\n!Alerm-ON!"
 			}
 
 			if timeNow.Second() == timeNowBefore.Second() {
 				// 何もしない
 			} else {
+				// 情報に変更があれば表示内容を更新する
 				labelTimeNow.FillScreen(glay)
 				tinyfont.WriteLine(labelTimeNow, &freemono.Regular12pt7b, 0, 18, timeNowString, white)
 				display.DrawRGBBitmap(0, 0, labelTimeNow.Buf, labelTimeNow.W, labelTimeNow.H)
 			}
 		} else {
+			// 時間設定モード
 			timeAlermString := fmt.Sprintf("setting alerm\n%02d:%02d", alermHour, alermMinute)
 
 			if modeBefore == 1 {
+				// 画面遷移直後の処理
 				display.FillScreen(black)
 				SettingAlermlabel.FillScreen(glay)
 				tinyfont.WriteLine(SettingAlermlabel, &freemono.Regular12pt7b, 0, 18, timeAlermString, white)
@@ -164,6 +168,7 @@ func main() {
 			if timeAlermString == timeAlermStringBefore {
 				// 何もしない
 			} else {
+				// 情報に変更があれば表示内容を更新する
 				SettingAlermlabel.FillScreen(glay)
 				tinyfont.WriteLine(SettingAlermlabel, &freemono.Regular12pt7b, 0, 18, timeAlermString, white)
 				display.DrawRGBBitmap(0, 0, SettingAlermlabel.Buf, SettingAlermlabel.W, SettingAlermlabel.H)
